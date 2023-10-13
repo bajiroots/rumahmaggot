1a1e57ed9d71a8375bf3a3eff7b0ab69caee9595
bajiroots <dickyarmansyah007@gmail.com>
2023-10-13 20:59:08 +0800

    initial commit

---
+++ app/Models/User.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Models;
+
+use Eloquent as Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+/**
+ * Class User
+ * @package App\Models
+ * @version October 13, 2023, 12:18 pm UTC
+ *
+ * @property string $name
+ * @property string $email
+ * @property string|\Carbon\Carbon $email_verified_at
+ * @property string $password
+ * @property string $role
+ * @property string $remember_token
+ */
+class User extends Authenticatable
+{
+    use SoftDeletes;
+
+    use HasFactory;
+
+    public $table = 'users';
+    
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+
+
+    protected $dates = ['deleted_at'];
+
+
+
+    public $fillable = [
+        'name',
+        'email',
+        'email_verified_at',
+        'password',
+        'role',
+        'remember_token'
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'name' => 'string',
+        'email' => 'string',
+        'email_verified_at' => 'datetime',
+        'password' => 'string',
+        'role' => 'string',
+        'remember_token' => 'string'
+    ];
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public static $rules = [
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|max:255',
+        'email_verified_at' => 'nullable',
+        'password' => 'required|string|max:255',
+        'role' => 'required|string',
+        'remember_token' => 'nullable|string|max:100',
+        'created_at' => 'nullable',
+        'updated_at' => 'nullable'
+    ];
+
+    public function getAuthIdentifierName()
+    {
+        return 'id'; // Change this if the unique identifier column in your table has a different name
+    }
+    
+}
